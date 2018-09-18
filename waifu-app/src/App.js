252e3a9ff4bd992538a14289a78b2466cb33a536
Oscar Morales <oemc_2404@outlook.com>
2018-09-18 15:23:21 -0600

    Change Logo

--- waifu-app/src/App.js
+++ waifu-app/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import logo from './Logo0.png';
-import logo1 from './Logo1.png';
+import logo from './Logo.png';
 import './App.css';
 import waifuList from './testList.json';
 import Navigation from './Navigation.js';
 import {BrowserRouter} from 'react-router-dom';
 
 class App extends Component {
+  
   updateList = () => {
     let allId = []
     waifuList.forEach((character) =>{
@@ -21,13 +21,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <div className="Logo" onClick={this.updateList}>
-            <img src={logo} className="App-logo" alt="app-logo" />
-            <img src={logo1} className="Animation-logo" alt="animation-logo" />
-          </div>
+          <img src={logo} className="Logo" alt="app-logo" onClick={this.updateList}/>
           <div className="Message">
             <h1 className="App-title">Welcome to WaifuApp!!!</h1>
-            <h3>To load the characters to LocalStorage click the React logo</h3>
+            <h3>To load the characters to LocalStorage click the logo</h3>
           </div>
         </header>
         <BrowserRouter>
