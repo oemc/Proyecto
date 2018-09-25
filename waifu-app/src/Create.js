3a019c234d41843ad502849d81caa5238b50022a
Oscar Morales <oemc_2404@outlook.com>
2018-09-25 10:31:21 -0600

    Create/Update

--- waifu-app/src/Create.js
+++ waifu-app/src/Create.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
 import './Create.css'
 import FileBase64 from 'react-file-base64';
+import uuidv4 from 'uuid/v4';
+import {Redirect} from 'react-router-dom';
 
 class Create extends Component{
     constructor(props){
         super(props);
-        let preState = {};
-        props.character != null ? preState.character = props.character : preState.character = {};
-        this.state = preState;
+        this.state = {
+            "done": false,
+            "character": props.character != null ? props.character : {}
+        };
         this.handleChange = this.handleChange.bind(this);
     }
     
@@ -23,7 +26,22 @@ class Create extends Component{
         this.setState({"character": updated});
     }
 
+    submit(){
+        let updated = Object.assign({}, this.state.character);
+        if(updated.id == null){
+            updated.id = uuidv4();
+            let foundId = JSON.parse(localStorage.getItem("allId"));
+            foundId.push(updated.id);
+            localStorage.setItem("allId",JSON.stringify(foundId));
+        }
+        localStorage.setItem(updated.id, JSON.stringify(updated));
+        this.setState({"done": true});
+    }
+
     render(){
+        if(this.state.done){
+            return(<Redirect to="/"/>);
+        }
         return(
             <div className="CharacterContainer">
                 <div className="ParamContainer">
@@ -33,11 +51,12 @@ class Create extends Component{
                     <div className="Parameter"><label>Occupation:</label><input name="occupation" value={this.state.character.occupation} onChange={this.handleChange}/></div>
                     <div className="Parameter"><label>Hair Color:</label><input name="hairColor" value={this.state.character.hairColor} onChange={this.handleChange}/></div>
                     <div className="Parameter"><label>Alias:</label><input name="alias" value={this.state.character.alias} onChange={this.handleChange}/></div>
-                    <div className="SubmitButton">Submit</div>
+                    <div className="SubmitButton" onClick={this.submit.bind(this)}>Submit</div>
                 </div>
                 <div>
                     <img className="Pic" src={this.state.character.pic} alt="character"/>
                 </div>
+                
             </div>
         );
     }
