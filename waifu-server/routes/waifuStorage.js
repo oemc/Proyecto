c88d77ef98f963080fd096e18366f8bd2b772ff3
Oscar Morales <oemc_2404@outlook.com>
2018-10-16 12:57:02 -0600

    User and password

--- waifu-server/routes/waifuStorage.js
+++ waifu-server/routes/waifuStorage.js
@@ -1,7 +1,7 @@
 var uuidv4 = require('uuid/v4');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/local');
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DB}`, {useNewUrlParser: true});
 
 const Schema = mongoose.Schema;
 const Character = new Schema({
