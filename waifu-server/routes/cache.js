98067a2c363d1d00532d8c3dafd1615d70a3db88
Oscar Morales <oemc_2404@outlook.com>
2018-11-04 02:19:17 -0600

    mongo + redis in Dockerfile

--- waifu-server/routes/cache.js
+++ waifu-server/routes/cache.js
@@ -1,7 +1,7 @@
 const redis = require('redis');
 
 function redisConnect(){
-    let client = redis.createClient({
+    let client = redis.createClient(`redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,{
         retry_strategy: ()=>{
             return new Error('The server refused the connection');
         }
