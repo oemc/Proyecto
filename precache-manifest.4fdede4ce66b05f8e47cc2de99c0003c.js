7fbc26043fd5da5005cc64cd3557a7ec8aa2feb5
Oscar Morales <oemoralesc@correo.url.edu.gt>
2019-04-24 18:19:32 -0600

    Build

---
+++ precache-manifest.4fdede4ce66b05f8e47cc2de99c0003c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81b1df9f65ffee5b9a0bcc899307965",
+    "url": "/proyecto/index.html"
+  },
+  {
+    "revision": "f06e46526d5c4ad80ff3",
+    "url": "/proyecto/static/css/main.bd86c4a5.chunk.css"
+  },
+  {
+    "revision": "f6e60a112b8f52a50dca",
+    "url": "/proyecto/static/js/2.1e08986a.chunk.js"
+  },
+  {
+    "revision": "f06e46526d5c4ad80ff3",
+    "url": "/proyecto/static/js/main.92741725.chunk.js"
+  },
+  {
+    "revision": "0cc234dbaab6b1b6743f",
+    "url": "/proyecto/static/js/runtime~main.b0b642ab.js"
+  },
+  {
+    "revision": "1732cd389f26c9f3f7e0b3473680b1af",
+    "url": "/proyecto/static/media/delete.1732cd38.svg"
+  },
+  {
+    "revision": "0544b6b462aa4ce285ccd15249590b5e",
+    "url": "/proyecto/static/media/edit.0544b6b4.svg"
+  }
+]);
