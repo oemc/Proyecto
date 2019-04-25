b7a9cf384aa235cfd6347bb8e8de6cec4367dc79
Oscar Morales <oemc_2404@outlook.com>
2019-04-24 22:44:03 -0600

    Updates

---
+++ precache-manifest.41e46642d8bc62a36d145db9f2732c74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e58d051da53fb9522685fe86234c04",
+    "url": "/Proyecto/index.html"
+  },
+  {
+    "revision": "c1535d347eb142c475b4",
+    "url": "/Proyecto/static/css/main.bd86c4a5.chunk.css"
+  },
+  {
+    "revision": "f6e60a112b8f52a50dca",
+    "url": "/Proyecto/static/js/2.1e08986a.chunk.js"
+  },
+  {
+    "revision": "c1535d347eb142c475b4",
+    "url": "/Proyecto/static/js/main.7c3bdfd7.chunk.js"
+  },
+  {
+    "revision": "5e7e97e207b3b66ddc2f",
+    "url": "/Proyecto/static/js/runtime~main.4c827175.js"
+  },
+  {
+    "revision": "1732cd389f26c9f3f7e0b3473680b1af",
+    "url": "/Proyecto/static/media/delete.1732cd38.svg"
+  },
+  {
+    "revision": "0544b6b462aa4ce285ccd15249590b5e",
+    "url": "/Proyecto/static/media/edit.0544b6b4.svg"
+  }
+]);
